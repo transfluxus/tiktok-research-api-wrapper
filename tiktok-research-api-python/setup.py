74515dbf6deab74d72f751fa7cc77fea4d4949be
carlos.avalos <carlos.avalos@bytedance.com>
2024-12-02 14:56:46 -0800

    initial release

--- tiktok-research-api-python/setup.py
+++ tiktok-research-api-python/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="TiktokResearchApi",
+    name="TikTokResearchApi",
     version="1.0.0",
     description="A Python package for interacting with the TikTok Research API",
     long_description=open("README.md").read(),
